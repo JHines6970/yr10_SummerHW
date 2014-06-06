4560579132a15c07b74e187cd3152ef4f9f88ba4
JHines6970 <jhines6970@sohamcollege.org.uk>
2014-06-06 14:54:29 +0100

    update of tables.py

--- HW/2/tables.py
+++ HW/2/tables.py
@@ -1,1 +1,26 @@
-#paste code here
+
+number2=0
+number=0
+x=0
+Finish=False
+WO=0
+
+while Finish == False:
+    number=0
+    x=1
+    while x > 0:
+        try:
+            number=int(input("Enter a number >>>"))
+        except ValueError:
+            print("Please enter a number!")
+            continue
+        else:
+            print("Number accepted.")
+            print()
+            x=0
+    for x in range(0,10):
+        WO=WO+1
+        number2=number2+number
+        print(str(number)+" X "+(str(WO)+" = "+str(number2)))
+    Finish=True
+    
